860ce36881f8a643fdaa86b5785c41b3b8e00fca
Nin Dang <nindang12345@gmail.com~>
2024-08-21 14:30:02 +0700

    fisrt

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+export default function Siderber(){
+    return(
+        <div className="text-red-500 font-semibold ">
+            <img width={20} src="/assets/home.svg" alt="icon" />
+            <h1>Siderber</h1>
+            <p>This is the siderber component</p>
+        </div>
+    )
+}
