80ce7faba3b31f9fb1169304cd99e789afd3322b
Nin Dang <nindang12345@gmail.com~>
2024-08-30 20:45:59 +0700

    update

--- src/components/RowThreads.tsx
+++ src/components/RowThreads.tsx
@@ -1,9 +1,12 @@
+"use client"
 import Link from "next/link"
+import { useState } from "react"
 export default function RowThreadss(){
     return(
+
         <div className="flex ">
             <button className="w-[210px] h-[46px] border-solid border-b-[1px] border-black text-sm font-bold">
-               <Link className="" href={'/profile'}>Thread</Link> 
+               <Link onClick={()=>updateToggle(1)} className="" href={'/profile'}>Thread</Link> 
             </button>
             <button className="w-[210px] h-[46px] border-solid border-b-[1px] text-gray-400 text-sm font-bold">
                 <Link href={'/threadreplied'}>Thread trả lời</Link> 
