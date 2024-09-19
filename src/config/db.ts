36754e5599f2a3658e20a92076cc419671802b51
louisdevzz <vohuunhan1310@gmail.com>
2024-09-19 17:10:42 +0700

    feat

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,8 +4,8 @@ const createDbConnection = async () => {
   try {
     const connection = await mysql.createConnection({
       host: 'localhost',
-      user: 'nindang',
-      password: 'nindang123',
+      user: 'louisdevzz',
+      password: 'Vohuunhan1310@',
       database: 'nexusocial'
     });
 
